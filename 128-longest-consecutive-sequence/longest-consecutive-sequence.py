998676455c8ee214d85ec80ab3386d0594778ca6
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-08-10 18:46:32 -0500

    Time: 328 ms (95.19%) | Memory: 31.8 MB (72.77%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -0,0 +1,41 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        """
+            Understand
+                - The purpose is to return the len of the longest consecutive elements
+
+                Questions
+                    - Can we have an array with no consecutive elements?
+                    - Can we have an empty array?
+            
+            Match
+                - Set to have only unique numbers
+            
+            Plan
+                - Convert into a set
+                - Checkf if lenght of the set is 0
+                - Initialize a variable to keep track of the max length 
+                - Traverse through the set
+                - Check if there is a current_number - 1
+                    - Not, set the temporary number to the number
+                    - Check if temporaryNumber + 1 is in setNums
+                        - Increment length
+                        - Make temporaryNumber to be equal to temporaryNumber + 1
+                    - Get the max length
+
+        """
+        set_nums = set(nums)
+        length_max = 0
+
+        for num in set_nums:
+            if num - 1 not in set_nums:
+                temporary_num = num
+                length = 1
+
+                while temporary_num + 1 in set_nums:
+                    temporary_num = temporary_num + 1
+                    length += 1
+                
+                length_max = max(length_max, length)
+            
+        return length_max
