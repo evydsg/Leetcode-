2840d5446b553ba9b73d488aab1c498e21165c82
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-09-02 19:07:12 -0700

    Time: 744 ms (50.43%) | Memory: 27.4 MB (90.80%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -2,59 +2,35 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         """
             Understand
-                The idea is to calculate the day to buy in which the price is at it lowest and you can sell at highest
+                The idea is to find the maximum profit I have from buying a stock and later selling it 
+                Thus to say that the day where we buy must have a lower price than the one we sell
 
-                Questions
-                    Can we have an empty array?
-                    Can we have an array with negative integers?
+                Can we have an empty array?
+                Can we have an array with negative elements?
+                Can we have duplicates?
             
-                Edge cases  
-                    prices = [7,1,5,3,6,4]
-                    price[0] = 7
-                    price[-1] = 4
-
-                    if price[left] > price[right]:
-                        left += 1
-                    if price[left] < price[right]:
-                        max_profit = max(max_profit, profit)
-                
-            Match 
-                Find minimum price
+            Match
+                Sliding Window
             
             Plan
-                Check if list is empty
-                    if len(prices) == 0:
-                        return 0
-                Initialize a variable to keep track of minimum price
-                    min_price = float("inf")
-                Initialize a variable to keep track of max profit
-                    max_profit = 0
-                Traverse through the list
-                    for price in prices:
-                    Check if current price is less than min_price
-                    Calculate the profit 
-                        profit = price - min_price
-                    Update the profit to have the max_profit
-                        max_profit = max(profit, max_profit)
-                
-            Evaluate    
-                Time Complexity
-                    O(n) since we traverse through the array 
-                Space Complexity
-                    O(1), we simply use one variable to keep track of the max price
-
+                Initialize a left pointer at the beginning of the array, variable to keep track of the maximum profit
+                Iterate through the prices
+                    Check if left pointer is less than the right pointer
+                        if so, increment the left pointer
+                    Calculate the maximum profit
+                        profit = prices[right] - profit[left]
+                    Update the profit to have the maximum profit found so far
+            
+            Evaluate
+                Time Complexity: O(n)
+                Space Complexity: O(1)
         """
-        if len(prices) == 0:
-            return 0
-        
-        min_price = float("inf")
-        max_profit = 0
+        left_ptr, max_profit = 0, 0
 
-        for price in prices:
-            min_price = min(price, min_price)
-
-            profit = price - min_price
-
-            max_profit = max(profit, max_profit)
+        for index in range(len(prices)):
+            while prices[left_ptr] > prices[index]:
+                left_ptr += 1
+            
+            max_profit = max(max_profit, prices[index] - prices[left_ptr])
         
         return max_profit
