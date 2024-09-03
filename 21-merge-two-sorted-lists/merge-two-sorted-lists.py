bc33698bcabb08ac13bb174540ccf6c71e599738
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-09-03 13:20:35 -0700

    Time: 30 ms (94.40%) | Memory: 16.6 MB (35.54%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -6,54 +6,49 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         """
-            Understand 
-                - The idea is to return one singly linked list that merges the two lists
+            Understand
+                The idea is to merge two lists into one and sorted?
 
-                Questions
-                    - Can we have two empty lists?
-                    - Can we have a one empty list?
-
+                Can we get empty lists?
+                Are we always guaranteed to get a sorted list?
+            
             Match
-                - Define a new list
-
+                LinkedList
+            
             Plan
-                - Check if both lists are empty
-                - Check if one of the lists is empty
-                - Create an empty node
-                - Assign current to the empty node
-                - Traverse through the list1 and list2
-                - Check what list is not None
-
-            Evaluate
-                - Time Complexity: O(n * m) since we traverse through the two lists
-                - Space Complexity: O(n) due to the new list created
+                Check if any of the lists is empty
+                Create a temporary node
+                Iterate through the lists while both have them have a value
+                    Compare current value of both lists to find the lowest
+                        Move the lowest to the next node and assign the current node
+                Check if any of the lists still have values
+                Return the temporary node.next
         """
         if list1 is None and list2 is None:
             return list1
-        
+
         if list1 is None:
             return list2
         
         if list2 is None:
             return list1
-
-        mergedList = ListNode(0)
-        current = mergedList
+        
+        merged_list = ListNode(0)
+        current_merged_list = merged_list
 
         while list1 and list2:
             if list1.val < list2.val:
-                current.next = list1
+                current_merged_list.next = list1
                 list1 = list1.next
-            else:
-                current.next = list2
+            else :
+                current_merged_list.next = list2
                 list2 = list2.next
-
-            current = current.next
+            
+            current_merged_list = current_merged_list.next
         
-        if list1 is not None:
-            current.next = list1
+        if list1:
+            current_merged_list.next = list1
+        elif list2:
+            current_merged_list.next = list2
         
-        elif list2 is not None:
-            current.next = list2
-        
-        return mergedList.next
+        return merged_list.next
