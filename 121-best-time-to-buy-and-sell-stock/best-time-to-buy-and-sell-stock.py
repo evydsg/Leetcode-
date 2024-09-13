45c446ca5285e1adcd506015a208b37d92d49860
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-09-13 16:57:39 -0400

    Time: 791 ms (15.11%) | Memory: 27.5 MB (11.02%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,36 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        """
-            Understand
-                The idea is to find the maximum profit I have from buying a stock and later selling it 
-                Thus to say that the day where we buy must have a lower price than the one we sell
+        left, index = 0, 0
+        maxProfit = 0
 
-                Can we have an empty array?
-                Can we have an array with negative elements?
-                Can we have duplicates?
+        while index < len(prices):
+            while prices[left] > prices [index]:
+                left += 1
             
-            Match
-                Sliding Window
-            
-            Plan
-                Initialize a left pointer at the beginning of the array, variable to keep track of the maximum profit
-                Iterate through the prices
-                    Check if left pointer is less than the right pointer
-                        if so, increment the left pointer
-                    Calculate the maximum profit
-                        profit = prices[right] - profit[left]
-                    Update the profit to have the maximum profit found so far
-            
-            Evaluate
-                Time Complexity: O(n)
-                Space Complexity: O(1)
-        """
-        left_ptr, max_profit = 0, 0
-
-        for index in range(len(prices)):
-            while prices[left_ptr] > prices[index]:
-                left_ptr += 1
-            
-            max_profit = max(max_profit, prices[index] - prices[left_ptr])
+            maxProfit = max(maxProfit, (prices[index]- prices[left]))
+            index += 1
         
-        return max_profit
+        return maxProfit
