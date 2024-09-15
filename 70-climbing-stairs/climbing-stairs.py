f74353d8c9ae589fca00397d03ff16a5e6285118
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-09-14 17:38:40 -0700

    Time: 24 ms (97.77%) | Memory: 16.4 MB (61.78%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,9 +2,11 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         one, two = 1, 1
 
-        for i in range(n-1):
-            temporary = one
+        for step in range(n-1):
+            temp = one
             one = one + two
-            two = temporary
+            two = temp
         
         return one
+
+        
