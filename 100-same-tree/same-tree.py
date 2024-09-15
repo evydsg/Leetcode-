8890c969d7fe146edb6da83ab52cd193a68a7119
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-09-15 16:44:41 -0700

    Time: 35 ms (55.04%) | Memory: 16.5 MB (37.13%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,38 +6,32 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        """
-            Understand
-                - The idea of the exercise is to check if two trees are the same
-                
-                Questions
-                    - Can we have empty lists?
-            
-            Match
-                - Binary Tree Traversal
-            
-            Plan
-                - Check if any of the roots if empty
-                - Traverse through the tree
-                    - Check if the current value of the nodes are the same
-                        - No, return false
-            
-            Evaluate
-                - Time Complexity: O(n)
-                - Space Complexity: O(n)
-
-        """
-
         if p is None and q is None:
             return True
         
-        if p is None:
-            return False
-            
-        if q is None:
+        if p is None or q is None:
             return False
         
         if p.val != q.val:
             return False
         
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        queue1, queue2 = [p], [q]
+
+        while queue1 and queue2:
+            current1, current2 = queue1.pop(0), queue2.pop(0)
+
+            if current1.val != current2.val:
+                return False
+            
+            if current1.left and current2.left:
+                queue1.append(current1.left)
+                queue2.append(current2.left)
+            elif current1.left or current2.left:
+                return False
+            if current1.right and current2.right:
+                queue1.append(current1.right)
+                queue2.append(current2.right)
+            elif current1.right or current2.right:
+                return False
+        
+        return True
