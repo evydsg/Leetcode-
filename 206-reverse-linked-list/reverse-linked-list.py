fb84f051d8c083cf6a5875a86fafed7108f796e6
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-09-16 10:25:59 -0700

    Time: 31 ms (89.75%) | Memory: 17.6 MB (84.98%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -1,43 +1,17 @@
-# Definition for singly-linked list node.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        """
-        Reverse a singly linked list.
-        
-        :param head: The head node of the singly linked list.
-        :return: The head node of the reversed singly linked list.
-        """
+        prev, current = None, head
 
-        # If the list is empty, return the head (which is None)
-        if head is None:
-            return head
-
-        # Initialize previous node as None (this will become the new tail)
-        previous = None
-
-        # Initialize current node as the head of the list
-        current = head
-
-        # Traverse through the list
         while current:
-            # Temporarily store the next node
-            next_node = current.next
-
-            # Reverse the current node's pointer
-            current.next = previous
-
-            # Move the previous node to this current node
-            previous = current
-
-            # Move to the next node in the original list
-            current = next_node
+            nextN = current.next
+            current.next = prev
+            prev = current
+            current = nextN
         
-        # At the end, previous will be the new head of the reversed list
-        return previous
-
+        return prev
         
