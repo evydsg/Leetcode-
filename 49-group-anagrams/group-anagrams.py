33c9471ec6318931bd74d1fa0037f57f2fbd5bdc
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-09-16 09:00:31 -0700

    Time: 102 ms (16.65%) | Memory: 22.1 MB (23.77%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,36 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        """
-            Understand
-                The idea is to find the anagrams and group them all together
+        result = defaultdict(list)
 
-                Can we have a list with no anagrams?
-                Are we always guaranteed to receive lowercase letters?
-                Can we receive an empty list?
-            
-            Match
-                Dictionary 
-            
-            Plan
-                Initialize a dictionary
-                Iterate through the list
-                    Sort each word to use it as the key
-                        Check if the key already exists in the dictionary
-                    Add the key to the dictionary
-                Return the values of the dictionary as a list
-            
-            Evaluate
-                Time Complexity: O(n)
-                Space Complexity: O(n)
-        """
-        dictionary = {}
+        for string in strs:
+            count = [0]*26
 
-        for word in strs:
-            key = "".join(sorted(word))
-
-            if key in dictionary:
-                dictionary[key].append(word)
-            else:
-                dictionary[key] = [word]
+            for character in string:
+                count[ord(character) - ord('a')] += 1
+            
+            result[tuple(count)].append(string)
         
-        return list(dictionary.values())
+        return result.values()
+        
