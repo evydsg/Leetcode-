b48b1768b6b0da724f5462428e6420fdf906766f
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-09-22 10:45:12 -0700

    Time: 335 ms (87.73%) | Memory: 31.8 MB (32.28%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,45 +1,17 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        """
-            Understand
-                - The purpose is to return the len of the longest consecutive elements
+        nums = set(nums)
+        result = 0
 
-                Questions
-                    - Can we have an array with no consecutive elements?
-                    - Can we have an empty array?
-            
-            Match
-                - Set to have only unique numbers
-            
-            Plan
-                - Convert into a set
-                - Checkf if lenght of the set is 0
-                - Initialize a variable to keep track of the max length 
-                - Traverse through the set
-                - Check if there is a current_number - 1
-                    - Not, set the temporary number to the number
-                    - Check if temporaryNumber + 1 is in setNums
-                        - Increment length
-                        - Make temporaryNumber to be equal to temporaryNumber + 1
-                    - Get the max length
-            
-            Evaluate
-                - Time Complexity: O(n), since we traverse through the set
-                - Space Complexity: O(n), since we use a set to avoid having duplicates
+        for num in nums:
+            if num - 1 not in nums:
+                current = num
+                count = 1
 
-        """
-        set_nums = set(nums)
-        length_max = 0
-
-        for num in set_nums:
-            if num - 1 not in set_nums:
-                temporary_num = num
-                length = 1
-
-                while temporary_num + 1 in set_nums:
-                    temporary_num = temporary_num + 1
-                    length += 1
+                while current + 1 in nums:
+                    current = current + 1
+                    count += 1
                 
-                length_max = max(length_max, length)
-            
-        return length_max
+                result = max(count, result)
+        
+        return result
