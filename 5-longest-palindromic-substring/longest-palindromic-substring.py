b2d197e1116e07c68297437b6b5445033efd1f4b
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-09-26 15:38:11 -0700

    Time: 359 ms (52.03%) | Memory: 16.4 MB (93.50%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,13 +1,33 @@
+"""
+    Understand: The idea is to return the longest palindrome that can be found in the string
+        - Can the string be empty?
+        - Are we always guaranteed to have a palindrome?
+
+    Match
+        Two Pointers
+    
+    Plan
+        Initialize a variable to keep track of the result
+        Traverse through the string
+        Initialize the two pointers
+            While character at left is equal to right
+                Continue adding
+                Move pointers
+            Keep the longest substring
+        Return substring
+
+"""
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        result = ""
-
+        substring = ""
+        
         for index in range(len(s)):
             left, right = index, index
 
             while left >= 0 and right < len(s) and s[left] == s[right]:
-                if right - left + 1 > len(result):
-                    result = s[left: right + 1]
+                if right - left + 1 > len(substring):
+                    substring = s[left:right + 1]
                 
                 left -= 1
                 right += 1
@@ -15,10 +35,10 @@ class Solution:
             left, right = index, index + 1
 
             while left >= 0 and right < len(s) and s[left] == s[right]:
-                if right - left + 1 > len(result):
-                    result = s[left: right + 1]
+                if right - left + 1 > len(substring):
+                    substring = s[left: right + 1]
                 
                 left -= 1
                 right += 1
-            
-        return result
+        
+        return substring
