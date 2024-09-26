afb0a52009655fc0e2c0d8d05d0bab5fdef0c60c
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-09-26 16:39:37 -0700

    Time: 45 ms (75.50%) | Memory: 16.9 MB (92.06%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,19 +3,20 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        dictionaryS, dictionaryT = {}, {}
+        dictionary = {}
 
         for character in s:
-            if character in dictionaryS:
-                dictionaryS[character] += 1
+            if character in dictionary:
+                dictionary[character] += 1
             else:
-                dictionaryS[character] = 1
+                dictionary[character] = 1
         
         for character in t:
-            if character in dictionaryT:
-                dictionaryT[character] += 1
-            else:
-                dictionaryT[character] = 1
+            if character in dictionary:
+                dictionary[character] -= 1
+
+                if dictionary[character] == 0:
+                    del dictionary[character]
+            
         
-        return dictionaryT == dictionaryS
-        
+        return len(dictionary) == 0
