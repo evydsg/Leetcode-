5fda2af7b7e6922d2a1f4bd5710287d435d2bbb5
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-09-26 15:52:43 -0700

    Time: 633 ms (80.87%) | Memory: 20.8 MB (19.44%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,10 +1,37 @@
+"""
+    Understand: the idea is to check if we can find distinct numbers that can add up to 0
+
+        - Can we have an empty array?
+        - Is the array sorted?
+        - Can we have only duplicates?
+    
+    Match
+        Two Pointers
+    
+    Plan
+        Initialize a variable to return the result
+        Sort the array
+        Iterate the list
+            Initialize two pointers
+                left starts after the index
+                right starts from the end
+
+                Calculate the sum as long as left pointer and right have not meet
+                    if the sum matches 0
+                        append to the result
+                    else
+                        Move pointers
+
+
+"""
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        triplets = []
         nums.sort()
+        result = []
 
         for index, number in enumerate(nums):
-            if index > 0 and number == nums[index-1]:
+            if index > 0 and number == nums[index - 1]:
                 continue
             
             left, right = index + 1, len(nums)-1
@@ -12,12 +39,12 @@ class Solution:
             while left < right:
                 threeSum = number + nums[left] + nums[right]
 
-                if threeSum < 0:
-                    left += 1
-                elif threeSum > 0:
+                if threeSum > 0:
                     right -= 1
+                elif threeSum < 0:
+                    left += 1
                 else:
-                    triplets.append([number, nums[left], nums[right]])
+                    result.append([number, nums[left], nums[right]])
 
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
@@ -26,6 +53,7 @@ class Solution:
                     
                     left += 1
                     right -= 1
-        
-        return triplets
-        
+            
+        return result
+
+                    
