70c2ddc6ea8ee8a053354b80e445204a46736273
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-09-29 10:57:40 -0700

    Time: 36 ms (80.75%) | Memory: 16.5 MB (88.98%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,8 +1,10 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
+        result = 0
+
         for index, number in enumerate(nums):
             nums[index] = str(number)
-        
+
         def compare(n1, n2):
             if n1 + n2 > n2 + n1:
                 return -1
@@ -10,4 +12,8 @@ class Solution:
                 return 1
         
         nums = sorted(nums, key = cmp_to_key(compare))
-        return str(int("".join(nums)))
+        
+        return (str(int("".join(nums))))
+        
+
+
