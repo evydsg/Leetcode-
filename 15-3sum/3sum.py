e2b006bfd71b2e062d275f7aaf7ba0a7eec2fa27
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-10-01 06:00:50 -0700

    Time: 627 ms (81.53%) | Memory: 20.8 MB (35.46%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,34 +1,7 @@
-"""
-    Understand: the idea is to check if we can find distinct numbers that can add up to 0
-
-        - Can we have an empty array?
-        - Is the array sorted?
-        - Can we have only duplicates?
-    
-    Match
-        Two Pointers
-    
-    Plan
-        Initialize a variable to return the result
-        Sort the array
-        Iterate the list
-            Initialize two pointers
-                left starts after the index
-                right starts from the end
-
-                Calculate the sum as long as left pointer and right have not meet
-                    if the sum matches 0
-                        append to the result
-                    else
-                        Move pointers
-
-
-"""
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        result = []
+        nums.sort() #logn
+        result = [] # O(1)
 
         for index, number in enumerate(nums):
             if index > 0 and number == nums[index - 1]:
@@ -45,9 +18,11 @@ class Solution:
                     left += 1
                 else:
                     result.append([number, nums[left], nums[right]])
+                    
 
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
+                    
                     while left < right and nums[right] == nums[right - 1]:
                         right -= 1
                     
@@ -55,5 +30,3 @@ class Solution:
                     right -= 1
             
         return result
-
-                    
