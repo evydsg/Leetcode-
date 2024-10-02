1e6d0b805989c6bc6d94b10a230e2d1c4599adbd
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-10-01 19:12:42 -0700

    Time: 38 ms (54.45%) | Memory: 16.8 MB (15.06%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -1,14 +1,14 @@
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
-        operations ={
-            '+': lambda x, y: x + y,
-            '-': lambda x, y: x - y,
-            '*': lambda x, y: x * y
+        operations = {
+            "+": lambda x, y: x + y,
+		    "-": lambda x, y: x - y,
+		    "*": lambda x, y: x * y
         }
 
         def backtrack(left, right):
             result = []
-            
+
             for index in range(left, right + 1):
                 operation = expression[index]
 
@@ -19,8 +19,10 @@ class Solution:
                     for n1 in nums1:
                         for n2 in nums2:
                             result.append(operations[operation](n1, n2))
+            
             if len(result) == 0:
-                result.append(int(expression[left : right + 1]))
+                result.append(int(expression[left:right + 1]))
+            
             return result
-        
+
         return backtrack(0, len(expression) - 1)
