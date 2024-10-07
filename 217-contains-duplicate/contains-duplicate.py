ad49caeebd6bf06264ecb60b9e4ae8942144e221
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-10-06 19:16:39 -0700

    Time: 419 ms (59.81%) | Memory: 34.6 MB (5.33%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,41 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        """
-            Understand
-                - The idea is to check if any of the values appear twice in the array
-
-                Questions
-                    - Are we always guaranteed to find duplicates in the list?
-                    - Can we have an empty list?
-                
-            Match
-                - Use dictionaries to keep track of how many times it appears
-            
-            Plan
-                - Check if list is empty
-                    - Yes, return list
-                - Initialize a dictionary
-                - Traverse through the list
-                    - If the element is not in the list
-                        - We add
-                    - Else
-                        Change the value to true
-            
-            Evaluate
-                - Time complexity: O(N)
-                - Space complexity: O(N)
-        """
-        
-        if nums is None:
-            return nums
-
-        dictionary = {}
+        count = {}
 
         for num in nums:
-            if num in dictionary:
-                dictionary[num] = True
+            if num in count:
+                count[num] += 1
                 return True
             else:
-                dictionary[num] = False
-        
+                count[num] = 1
+
         return False
