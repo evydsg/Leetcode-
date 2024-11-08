78f4c0f0aa16c41c7ace22ef2c7da233dd5426e8
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-11-08 05:58:57 -0800

    Time: 0 ms (100.00%) | Memory: 20.8 MB (35.36%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,27 +1,10 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        """
-            Understand
-                The idea is to merge all overlapping intervals
-
-                Can we have an empty list?
-                Are we always guaranteed to have overlapping intervals?
-                Can we have duplicates in the interval?
-            
-            Match
-                Two pointers
-            
-            Plan
-                Initialize a list to keep track of the result
-                Sort the list by using the first element
-
-
-        """
-        intervals.sort(key = lambda i:i[0])
+        intervals.sort(key=lambda intervals:intervals[0])
         result = [intervals[0]]
 
         for start, end in intervals[1:]:
-            lastEnd = result[-1][1] 
+            lastEnd = result[-1][1]
 
             if start <= lastEnd:
                 result[-1][1] = max(lastEnd, end)
@@ -29,4 +12,3 @@ class Solution:
                 result.append([start, end])
         
         return result
-        
