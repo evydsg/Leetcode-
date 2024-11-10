316ebb9e606efb1740cca5237cf29640865028d0
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-11-10 14:29:38 -0800

    Time: 0 ms (100.00%) | Memory: 16.8 MB (8.84%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -1,17 +1,17 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         differences = []
-        result = 0
+        cost = 0
 
         for cA, cB in costs:
             differences.append([cB - cA, cA, cB])
-        
+
         differences.sort()
-        
-        for index in range(len(costs)):
-            if index < len(differences)//2:
-                result += differences[index][2]
+
+        for index in range(len(differences)):
+            if index < len(differences) / 2:
+                cost += differences[index][2]
             else:
-                result += differences[index][1]
-        
-        return result
+                cost += differences[index][1]
+
+        return cost
