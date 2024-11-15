e6da48430aabd08d4df9f32289432b494562f2aa
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-11-15 04:21:17 -0800

    Time: 12 ms (7.44%) | Memory: 18.4 MB (99.19%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
@@ -4,15 +4,50 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+"""
+    Understand: add a value to the tree assuming that the value is not yet there
+        - First, find out the perfect location to insert the tree
+        - Second, create a new node
+
+        - Can the three be empty?
+        - Can the value be negative?
+
+    Match
+        BST Search
+    
+    Plan (Iteratively)
+        - Check if the tree is empty
+            - Create new Node
+        - Initialize a pointer to start from the root
+        - Iterate through the tree as long as current has a value
+            - Check if the value is less or greater than root
+                - Check if root has a value on one of the branches
+                    - No, create a new node
+                    - Yes, assign the next node to the next subtree
+        - Return root
+
+"""
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         if root is None:
             return TreeNode(val)
         
-        if val < root.val:
-            root.left = self.insertIntoBST(root.left, val)
-        elif val > root.val:
-            root.right = self.insertIntoBST(root.right, val)
+        current = root
+
+        while current:
+            if val < current.val:
+                if current.left is None:
+                    current.left = TreeNode(val)
+                    break
+                else:
+                    current = current.left
+            else:
+                if current.right is None:
+                    current.right = TreeNode(val)
+                    break
+                else:
+                    current = current.right
         
         return root
         
