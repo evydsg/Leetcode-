b727c5aec0c5adbf62cfeb5f42ebc839d567a312
Evelise Karen Guenda <evelisekguenda@gmail.com>
2024-11-20 05:50:56 -0800

    Time: 75 ms (13.12%) | Memory: 17.6 MB (42.87%) - LeetSync

--- 838-design-linked-list/design-linked-list.py
+++ 838-design-linked-list/design-linked-list.py
@@ -6,70 +6,84 @@ class Node:
 class MyLinkedList:
     def __init__(self):
         self.head = None
-        
+
     def get(self, index: int) -> int:
         current = self.head
         count = 0
-
+        
         while current:
             if count == index:
                 return current.value
+
             count += 1
             current = current.next
         
-        return -1 
+        return -1
 
     def addAtHead(self, val: int) -> None:
-        newHead = Node(val, self.head)
+        newHead = Node(val)
+        newHead.next = self.head
         self.head = newHead
         
 
     def addAtTail(self, val: int) -> None:
-        newTail = Node(val)
         if self.head is None:
-            self.head = newTail
+            self.head = Node(val)
             return
         
         current = self.head
+
         while current.next:
             current = current.next
-        current.next = newTail
+        
+        current.next = Node(val)
 
     def addAtIndex(self, index: int, val: int) -> None:
         if index == 0:
             self.addAtHead(val)
             return
 
-        count = 0
-        current = self.head
-        while count < index - 1 and current:
-            current = current.next
-            count += 1
-        
-        if current is None:
-            return
-        
-        newNode = Node(val, current.next)
-        current.next = newNode
+        else:
+            count = 0
+            current = self.head
 
+            while current and count < index - 1:
+                current = current.next
+                count += 1
+            
+            if current and current.next is not None:
+                nextNode = current.next
+                current.next = Node(val)
+                current = current.next
+                current.next = nextNode
 
+            elif current and current.next is None:
+                self.addAtTail(val)
+                return
+
+            elif current is None:
+                return
+            
     def deleteAtIndex(self, index: int) -> None:
         if index == 0:
             if self.head:
                 self.head = self.head.next
-            return
+                return
         
         count = 0
         current = self.head
-        while count < index - 1 and current:
-            current = current.next
-            count += 1
-        
-        if current is None or current.next is None:
-            return
-        
-        current.next = current.next.next
 
+        while current and count < index - 1:
+            count += 1
+            current = current.next
+        
+        if current and current.next:
+            current.next = current.next.next
+            
+        
+
+        
+        
 
 
 # Your MyLinkedList object will be instantiated and called as such:
